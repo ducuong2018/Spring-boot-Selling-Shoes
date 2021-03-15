3e4ed45915b1e9dff42de6177978dab8c2d117b2
Cuong DC <cuongdc@mcg-corp.vn>
2021-03-15 07:34:18 +0700

    sql

---
+++ shoes.sql
@@ -0,0 +1,9 @@
+create database Selling_Shoes
+use Selling_Shoes
+create table tbl_use(
+id int  AUTO_INCREMENT,
+username Nvarchar(50),
+passwork nvarchar(50),
+email nvarchar(50),
+primary key(id)
+)
